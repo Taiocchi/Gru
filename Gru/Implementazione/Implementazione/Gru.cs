dc9dbdcf39a002622c13f6b700c60149f230afdb
Luca Taiocchi <Taiocchi>
2024-10-21 13:53:30 +0200

    Versione 01

---
+++ Gru/Implementazione/Implementazione/Gru.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementazione
+{
+    internal class Gru
+    {
+        private string numeroSeriale;
+        private string produttore;
+        private int pesoMassimo;
+        private int altezzaMassima;
+        private int altezzaMinima;
+        private int altezzaBraccio;
+
+        public int getAltezzaBraccio
+        {
+
+        }
+        public string getNumeroSeriale
+        {
+
+        }
+
+        public string getProduttore
+        {
+
+        }
+
+        public int getPesoMassimo
+        {
+
+        }
+
+        public int getAltezzaMassima
+        {
+
+        }
+
+        public int getAltezzaMinima
+        {
+
+        }
+
+
+        public void alzaBraccio
+        {
+
+        }
+
+        public void abbassaBraccio
+        {
+
+        }
+
+        public void posizioneSicurezza
+        {
+
+        }
+
+
+        public Gru(string numeroSeriale, string produttore, int pesoMassimo, int altezzaMassima, int altezzaMinima)
+        {
+
+        }
+    }
+}
---
+++ Gru/Implementazione/Implementazione/Stazione.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementazione
+{
+    internal class Stazione
+    {
+        private Gru gru;
+
+        private string numeroSeriale;
+
+        public string getNumeroSeriale
+        {
+
+        }
+
+        public void alzaBraccio
+        {
+
+        }
+
+        public void abbassaBraccio
+        {
+
+        }
+
+        public void posizioneSicurezza
+        {
+
+        }
+
+
+        public Stazione(string numeroSeriale)
+        {
+
+        }
+    }
+}
